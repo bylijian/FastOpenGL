909b68bb857de2c630f27dc34fb68b084f8a05e4
lij004 <lij004@mail.cmft.com>
2019-07-04 14:54:11 +0800

    [IMP]FastOpenGL Hello World-绘制一个三角形

--- app/src/main/java/com/bylijian/fastopengl/MainActivity.java
+++ app/src/main/java/com/bylijian/fastopengl/MainActivity.java
@@ -1,13 +1,121 @@
 package com.bylijian.fastopengl;
 
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * 演示绘制一个三角形的demo
+ * 参考： https://blog.piasy.com/2016/06/07/Open-gl-es-android-2-part-1/index.html
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        glSurfaceView = findViewById(R.id.glsurfaceview);
+        //设置EGL版本为2
+        glSurfaceView.setEGLContextClientVersion(2);
+        //设置Render
+        glSurfaceView.setRenderer(new MyRenderer());
+        //设置渲染模式
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    private static class MyRenderer implements GLSurfaceView.Renderer {
+
+        private static final String VERTEX_SHADER =
+                "attribute vec4 vPosition;\n"
+                        + "void main() {\n"
+                        + "  gl_Position = vPosition;\n"
+                        + "}";
+
+        private static final String FRAGMENT_SHADER =
+                "precision mediump float;\n"
+                        + "void main() {\n"
+                        + "  gl_FragColor = vec4(0.5, 0, 0, 1);\n"
+                        + "}";
+
+        private static final float[] VERTEX = {   // in counterclockwise order:
+                0, 1, 0,  // top
+                -0.5f, -1, 0,  // bottom left
+                1, -1, 0,  // bottom right
+        };
+
+        private final FloatBuffer mVertexBuffer;
+
+        private int mProgram;
+        private int mPositionHandle;
+
+        MyRenderer() {
+            mVertexBuffer = ByteBuffer.allocateDirect(VERTEX.length * 4)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .put(VERTEX);
+            mVertexBuffer.position(0);
+        }
+
+        static int loadShader(int type, String shaderCode) {
+            int shader = GLES20.glCreateShader(type);
+            GLES20.glShaderSource(shader, shaderCode);
+            GLES20.glCompileShader(shader);
+            return shader;
+        }
+
+        @Override
+        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+            mProgram = GLES20.glCreateProgram();
+            int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER);
+            int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+            GLES20.glAttachShader(mProgram, vertexShader);
+            GLES20.glAttachShader(mProgram, fragmentShader);
+            GLES20.glLinkProgram(mProgram);
+
+            GLES20.glUseProgram(mProgram);
+
+            mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+            GLES20.glEnableVertexAttribArray(mPositionHandle);
+            GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false,
+                    12, mVertexBuffer);
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 unused, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 unused) {
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+        }
+
     }
 }
